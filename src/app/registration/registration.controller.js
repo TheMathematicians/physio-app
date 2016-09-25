43e5fd9224bacc223598a97817d07e2b104cade4
Stevenson-4 <son.steven4@gmail.com>
2016-09-25 14:56:58 +0530

    Initial commit for registration screen

---
+++ src/app/registration/registration.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('physio')
+    .controller('RegistrationController', RegistrationController);
+
+  /** @ngInject */
+  function RegistrationController() {
+    //var vm = this;
+  }
+})();
